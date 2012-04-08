8f22efb3441841ad5a15ff81a5986a730a877a3e
Roger Raymond <roger@lynch-raymond.com>
2012-04-08 14:27:10 -0400

    adding include files

---
+++ header.inc.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    
+    <title>Freckled Frosting :: Gourmet Bakery</title>
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+
+    <link rel="stylesheet" href="styles/base.css">
+
+    <!--[if lt IE 9]>
+      <script src="scripts/html5
+    <![endif]-->
+
+  </head>
+
+
+</html>
